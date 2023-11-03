e5b8691babf72c7b7880ff62b00603ae67e8200d
Per <atlasfreak@gmx.de>
2023-11-03 02:40:27 +0100

    corrected app name

--- apps.py
+++ apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DarmstadttermineConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.darmstadtTermine"
+    name = "darmstadtTermine"
