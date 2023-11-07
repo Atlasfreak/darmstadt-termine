483de3478786e559f785efa65ea2cdd0d1ce36eb
Per <atlasfreak@gmx.de>
2023-11-07 01:36:48 +0100

    cleaned up admin panel

--- admin.py
+++ admin.py
@@ -1,36 +1,37 @@
 from django.contrib import admin
 
-from .models import Appointment, Notification, AppointmentType, AppointmentCategory
+from .models import Appointment, AppointmentCategory, AppointmentType, Notification
 
 
 @admin.register(Appointment)
 class AppointmentAdmin(admin.ModelAdmin):
-    list_display = ("id", "creation_date", "start_time", "end_time", "date")
+    date_hierarchy = "date"
+    list_display = ("creation_date", "start_time", "end_time", "date")
     list_filter = ("creation_date", "date")
-    raw_id_fields = ("appointment_type",)
+    # raw_id_fields = ("appointment_type",)
+    autocomplete_fields = ("appointment_type",)
 
 
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
     list_display = (
-        "id",
         "email",
         "creation_date",
         "last_sent",
         "minimum_waittime",
     )
     list_filter = ("creation_date", "last_sent")
-    raw_id_fields = ("appointment_type",)
+    autocomplete_fields = ("appointment_type",)
 
 
 @admin.register(AppointmentType)
 class AppointmentTypeAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "index", "appointment_category")
+    list_display = ("name", "index", "appointment_category")
     list_filter = ("appointment_category",)
     search_fields = ("name",)
 
 
 @admin.register(AppointmentCategory)
 class AppointmentCategoryAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "index")
+    list_display = ("name", "index")
     search_fields = ("name",)
