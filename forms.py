0a28d8baefd649b0f4947b65ef13461bc400e567
Per <atlasfreak@gmx.de>
2023-11-26 00:15:42 +0100

    added language field fixed syntax error refactored with utils functions

--- forms.py
+++ forms.py
@@ -9,37 +9,30 @@ from django.utils.http import urlsafe_base64_encode
 from django.utils.translation import gettext_lazy as _
 
 from .conf import settings
-from .models import Notification
-from .tokens import default_access_token_generator, default_activation_token_generator
+from .models import AppointmentType, Notification
+from .tokens import (
+    notification_access_token_generator,
+    notification_activation_token_generator,
+)
+from .utils.email import create_template_mail
+from .utils.site import get_site_name_domain
 
 
 class NotificationRegisterForm(forms.ModelForm):
     class Meta:
         model = Notification
-        fields = ("email",)
+        fields = ("email", "language")
 
-    def send_mail(
-        self,
-        subject,
-        email_template_name,
-        html_email_template,
-        context,
-        from_email,
-        to_email,
-    ):
-        text_body = loader.render_to_string(email_template_name, context)
-        html_body = loader.render_to_string(html_email_template, context)
-        email_message = EmailMultiAlternatives(
-            subject, text_body, from_email, [to_email]
-        )
-        email_message.attach_alternative(html_body, "text/html")
-
-        email_message.send()
+    language = forms.ChoiceField(
+        label=Meta.model._meta.get_field("language").verbose_name,
+        choices=settings.DARMSTADTTERMINE_AVAILABLE_LANGUAGES,
+        required=True,
+    )
 
     def save(
         self,
-        commit: bool = True,
-        token_generator=default_activation_token_generator,
+        commit=True,
+        token_generator=notification_activation_token_generator,
         email_template_name="darmstadtTermine/notification/activation_email_body.txt",
         html_email_template="darmstadtTermine/notification/activation_email_body.html",
         from_email=None,
@@ -49,9 +42,7 @@ class NotificationRegisterForm(forms.ModelForm):
     ) -> Notification:
         notification = super().save(commit)
 
-        current_site = get_current_site(request)
-        site_name = current_site.name
-        domain = current_site.domain
+        site_name, domain = get_site_name_domain(request)
 
         context = {
             "domain": domain,
@@ -61,10 +52,12 @@ class NotificationRegisterForm(forms.ModelForm):
             "token": token_generator.make_token(notification),
             "timeout": datetime.timedelta(
                 seconds=settings.DARMSTADTTERMINE_ACTIVATION_TIMEOUT
-            )
-            ** (extra_email_context or {}),
+            ),
+            "email_language": notification.language,
+            **(extra_email_context or {}),
         }
-        self.send_mail(
+
+        create_template_mail(
             _(
                 "Bitte bestätige deine E-Mail Adresse um Benachrichtigungen für Termine zu erhalten"
             ),
@@ -73,6 +66,6 @@ class NotificationRegisterForm(forms.ModelForm):
             context=context,
             from_email=from_email,
             to_email=notification.email,
-        )
+        ).send()
 
         return notification
