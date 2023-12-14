3e1a20382f4642b22ecefeaa4b665fdfb0652d9d
Per <atlasfreak@gmx.de>
2023-12-14 01:12:43 +0100

    changed create_notification_email_message to use the notification object added delete_token adapted toplevel templates added type hints

--- utils/email.py
+++ utils/email.py
@@ -1,43 +1,49 @@
 from django.core import mail
 from django.template import loader
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 from django.utils.translation import gettext_lazy as _
+
+from ..models import Notification
+from ..tokens import notification_delete_token_generator
+from .site import get_site_name_domain
 
 
 def create_notification_email_message(
-    protocol,
-    site_name,
-    domain,
-    email_adress,
-    appointments_count,
-    appointment_categories_list,
-    language,
+    protocol: str,
+    notification: Notification,
+    appointments_count: int,
+    appointment_categories_list: list,
 ):
+    site_name, domain = get_site_name_domain()
     context = {
         "appointments_count": appointments_count,
         "appointment_categories_list": appointment_categories_list,
         "site_name": site_name,
         "domain": domain,
         "protocol": protocol,
-        "email_language": language,
+        "email_language": notification.language,
+        "delete_token": notification_delete_token_generator.make_token(notification),
+        "idb64": urlsafe_base64_encode(force_bytes(notification.pk)),
     }
 
     return create_template_mail(
         _("Neue Termine für das Bürgerbüro Darmstadt verfügbar!"),
-        "darmstadtTermine/notification/email/notification_email_body.txt",
-        "darmstadtTermine/notification/email/notification_email_body.html",
+        "darmstadtTermine/email/notification_email_body.txt",
+        "darmstadtTermine/email/notification_email_body.html",
         context,
         None,
-        email_adress.email,
+        notification.email,
     )
 
 
 def create_template_mail(
-    subject,
-    txt_email_template,
-    html_email_template,
-    context,
-    from_email,
-    to_email,
+    subject: str,
+    txt_email_template: str,
+    html_email_template: str,
+    context: dict,
+    from_email: str,
+    to_email: str,
 ):
     text_body = loader.render_to_string(txt_email_template, context)
     html_body = loader.render_to_string(html_email_template, context)
