dba6ad516e290e04d6999751c0ce30c206977898
Per <atlasfreak@gmx.de>
2024-01-05 03:46:48 +0100

    add more typehints

--- utils/site.py
+++ utils/site.py
@@ -1,8 +1,8 @@
-from django.contrib.sites.models import Site
 from django.contrib.sites.shortcuts import get_current_site
+from django.http import HttpRequest
 
 
-def get_site_name_domain(request=None):
+def get_site_name_domain(request: HttpRequest = None):
     current_site = get_current_site(request)
     site_name = current_site.name
     domain = current_site.domain
--- utils/time.py
+++ utils/time.py
@@ -1,7 +1,9 @@
+from datetime import tzinfo
+
 from django.utils.timezone import make_aware
 
 
-def make_aware_no_error(value, timezone=None):
+def make_aware_no_error(value, timezone: tzinfo = None):
     """Make a datetime.datetime time zone aware and if it already is aware return it"""
     try:
         return make_aware(value, timezone)
