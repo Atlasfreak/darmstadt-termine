a5ae26e312cb0e05ecc2a70f3c0e8ef5deb0b3e9
Per Göttlicher <atlasfreak@gmx.de>
2024-04-02 17:26:10 +0200

    use new models and fields when scraping appointments

--- scraper.py
+++ scraper.py
@@ -9,12 +9,19 @@ from django.core.mail import mail_admins
 from django.db.models import Q
 from django.utils import timezone
 
-from .models import Appointment, AppointmentCategory, AppointmentType, ScraperRun
+from .models import (
+    Appointment,
+    AppointmentCategory,
+    AppointmentType,
+    Location,
+    ScraperRun,
+)
 from .utils.time import make_aware_no_error
 
 URL = (
     "https://tevis.ekom21.de/stdar/"  # Link zum Terminvergabe Tool der Stadt Darmstadt
 )
+time_forms = SoupStrainer("form", attrs={"class": "suggestion_form"})
 
 
 async def add_appointment(
@@ -22,6 +29,7 @@ async def add_appointment(
     end_time: datetime.time,
     date: datetime.date,
     appointment_type: AppointmentType,
+    location: Location,
 ):
     """
     add_appointment writes the appointment to the database
@@ -40,6 +48,7 @@ async def add_appointment(
         start_time=make_aware_no_error(start_time),
         end_time=make_aware_no_error(end_time),
         date=date,
+        location=location,
     )
     await appointment.appointment_type.aadd(appointment_type)
 
@@ -49,60 +58,65 @@ async def fetch_appointment(
     appointment_category: int,
     appointment_type: AppointmentType,
 ) -> list[Coroutine]:
-    request = await client.post(
-        "location",
-        params={
-            "mdt": appointment_category,
-            f"cnc-{appointment_type.index}": 1,
-        },
-        data={
-            "loc": 42,
-            "select_location": "Bürger-+und+Ordnungsamt+(Luisencenter)+auswählen",
-        },
-        follow_redirects=True,
-    )
-    try:
-        request.raise_for_status()
-    except httpx.HTTPStatusError as e:
-        mail_admins(
-            "Fehler beim Aufruf der Terminvergabe von Darmstadt",
-            f"Der Scraper hat, beim Versuch die Termine zu ermitteln, einen Verbindungsfehler erhalten:\n{e}",
+    async for location in appointment_type.location.all():
+        request = await client.post(
+            "location",
+            params={
+                "mdt": appointment_category,
+                f"cnc-{appointment_type.index}": 1,
+            },
+            data={
+                "loc": location.index,
+                "select_location": location.descriptor,
+            },
+            follow_redirects=True,
         )
-        raise e
-
-    time_forms = SoupStrainer("form", attrs={"class": "suggestion_form"})
-    soup = BeautifulSoup(request.text, "lxml", parse_only=time_forms)
-    tasks = []
-
-    for element in soup:
         try:
-            start_time = int(
-                element.findNext("input", attrs={"name": "start"})["value"]
-            )  # in minutes
-            end_time = int(
-                element.findNext("input", attrs={"name": "end"})["value"]
-            )  # in minutes
-            date: str = element.findNext("input", attrs={"name": "date"})[
-                "value"
-            ]  # format YYYYMMDD
-        except TypeError:
+            request.raise_for_status()
+        except httpx.HTTPStatusError as e:
             mail_admins(
-                "Fehler beim Parsen der Termine",
-                f"Das nachfolgende Terminelement konnte nicht geparst werden.\n{element}\nSoup:\n{soup}\nAnfragetext:\n{request.text}",
+                "Fehler beim Aufruf der Terminvergabe von Darmstadt",
+                f"Der Scraper hat, beim Versuch die Termine zu ermitteln, einen Verbindungsfehler erhalten:\n{e}",
             )
-            continue
-        tasks.append(
-            add_appointment(
-                start_time=datetime.time(minute=start_time % 60, hour=start_time // 60),
-                end_time=datetime.time(minute=end_time % 60, hour=end_time // 60),
-                date=datetime.datetime.strptime(date, "%Y%m%d").date(),
-                appointment_type=appointment_type,
+            raise e
+
+        soup = BeautifulSoup(request.text, "lxml", parse_only=time_forms)
+        tasks = []
+
+        for element in soup:
+            try:
+                start_time = int(
+                    element.findNext("input", attrs={"name": "start"})["value"]
+                )  # in minutes
+                end_time = int(
+                    element.findNext("input", attrs={"name": "end"})["value"]
+                )  # in minutes
+                date: str = element.findNext("input", attrs={"name": "date"})[
+                    "value"
+                ]  # format YYYYMMDD
+            except TypeError:
+                mail_admins(
+                    "Fehler beim Parsen der Termine",
+                    f"Das nachfolgende Terminelement konnte nicht geparst werden.\n{element}\nSoup:\n{soup}\nAnfragetext:\n{request.text}",
+                )
+                continue
+            tasks.append(
+                add_appointment(
+                    start_time=datetime.time(
+                        minute=start_time % 60, hour=start_time // 60
+                    ),
+                    end_time=datetime.time(minute=end_time % 60, hour=end_time // 60),
+                    date=datetime.datetime.strptime(date, "%Y%m%d").date(),
+                    appointment_type=appointment_type,
+                    location=location,
+                )
             )
-        )
     return tasks
 
 
-async def fetch_appointments(appointment_category: int, appointment_types):
+async def fetch_appointments(
+    department_index: int, appointment_category: int, appointment_types
+):
     """
     fetch_appointments looks for all available appointments of a specific type
 
@@ -115,7 +129,7 @@ async def fetch_appointments(appointment_category: int, appointment_types):
     async with httpx.AsyncClient(
         base_url=URL, headers={"user-agent": "Termin-Scraper/1.0"}, max_redirects=50
     ) as client:
-        await client.get("select2", params={"md": 4})
+        await client.get("select2", params={"md": department_index})
         await client.get(
             "location",
             params={
@@ -137,13 +151,14 @@ async def fetch_all_types():
     fetch_all_types fetches appointments for all types
     """
     appointment_categories = await sync_to_async(
-        AppointmentCategory.objects.prefetch_related("types").all
+        AppointmentCategory.objects.prefetch_related("types", "department").all
     )()
     scraper_run = ScraperRun()
     await scraper_run.asave()
     await asyncio.gather(
         *[
             fetch_appointments(
+                appointment_category.department.index,
                 appointment_category.index,
                 appointment_category.types.filter(active=True),
             )
