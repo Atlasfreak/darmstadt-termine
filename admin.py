eda2b6facaacea02714ce2c6a052613c17d38a50
Per Göttlicher <atlasfreak@gmx.de>
2024-04-02 16:46:40 +0200

    add new models and fields to admin

--- admin.py
+++ admin.py
@@ -4,6 +4,8 @@ from .models import (
     Appointment,
     AppointmentCategory,
     AppointmentType,
+    Department,
+    Location,
     Notification,
     ScraperRun,
 )
@@ -12,9 +14,14 @@ from .models import (
 @admin.register(Appointment)
 class AppointmentAdmin(admin.ModelAdmin):
     date_hierarchy = "date"
-    list_display = ("creation_date", "start_time", "end_time", "date")
+    list_display = (
+        "creation_date",
+        "start_time",
+        "end_time",
+        "date",
+        "location",
+    )
     list_filter = ("creation_date", "date")
-    # raw_id_fields = ("appointment_type",)
     autocomplete_fields = ("appointment_type",)
 
 
@@ -22,25 +29,28 @@ class AppointmentAdmin(admin.ModelAdmin):
 class NotificationAdmin(admin.ModelAdmin):
     list_display = (
         "email",
+        "language",
         "creation_date",
         "last_sent",
         "minimum_waittime",
         "active",
     )
-    list_filter = ("creation_date", "last_sent")
+    list_filter = ("creation_date", "last_sent", "active")
     autocomplete_fields = ("appointment_type",)
 
 
 @admin.register(AppointmentType)
 class AppointmentTypeAdmin(admin.ModelAdmin):
-    list_display = ("name", "index", "appointment_category", "active")
-    list_filter = ("appointment_category",)
+    list_display = ("name", "index", "active", "appointment_category")
+    list_filter = ("active", "appointment_category")
+    autocomplete_fields = ("location",)
     search_fields = ("name",)
 
 
 @admin.register(AppointmentCategory)
 class AppointmentCategoryAdmin(admin.ModelAdmin):
-    list_display = ("name", "index")
+    list_display = ("name", "index", "department")
+    list_filter = ("department",)
     search_fields = ("name",)
 
 
@@ -48,3 +58,15 @@ class AppointmentCategoryAdmin(admin.ModelAdmin):
 class ScraperRunAdmin(admin.ModelAdmin):
     list_display = ("start_time", "end_time")
     list_filter = ("start_time", "end_time")
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ("name", "descriptor", "index")
+    search_fields = ("name",)
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ("name", "index")
+    search_fields = ("name",)
